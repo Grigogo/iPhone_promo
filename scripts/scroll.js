61f6e6ee7e5af272097f460e5696ce0ffc640fbb
Grigogo <grigoriirozhkov@gmail.com>
2022-01-22 18:58:57 +0300

    first commit

---
+++ scripts/scroll.js
@@ -0,0 +1,47 @@
+const links = document.querySelectorAll('.header-menu__item a')
+seamless.polyfill();
+
+links.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        const id = element.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+        
+        if (section) {
+            seamless.elementScrollIntoView( section, {
+                behavior: 'smooth',
+                block: 'start'
+            })
+        } else {
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+});
+
+const details = document.querySelector('.card-details__link-characteristics')
+console.log(details)
+
+details.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const id =details.getAttribute('href').substring(1)
+    const section = document.getElementById(id)
+
+    if (section) {
+        seamless.elementScrollIntoView( section, {
+            behavior: 'smooth',
+            block: 'start'
+        })
+    } else {
+        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
+    }
+})
